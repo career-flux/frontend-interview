31d339f053a96b7c008696b2eeb3f246d451ce47
dango0812 <ehdrbsla12000@gmail.com>
2026-07-23 02:19:21 +0900

    feat: add icon components
    - add brand icons for JavaScript, TypeScript, and React categories
    - add UI icons for hamburger, close, and chevron

---
+++ src/components/icons/close.tsx
@@ -0,0 +1,19 @@
+export function CloseIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      {...props}
+    >
+      <title>Close</title>
+      <line x1="4" y1="4" x2="20" y2="20" />
+      <line x1="20" y1="4" x2="4" y2="20" />
+    </svg>
+  );
+}
